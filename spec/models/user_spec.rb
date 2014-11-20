e7a713c4f74c6e1497a4b7698a3060d84dd19d61
kaspernj <k@spernj.org>
2014-11-20 11:43:38 +0100

    Autodelete data when dependent models are deleted.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,22 @@ describe User do
   let!(:user){ create :user }
   let!(:order){ create :order, user: user }
 
+  before do
+    user.update_customized_data(facebook_email: "kaspernj@facebook.com")
+  end
+
   it "should be possible to set custom data and it shouldn't mix up" do
     user.update_customized_data(facebook_email: "kaspernj@facebook.com")
-    user.customized_data[:facebook_email].should eq "kaspernj@facebook.com"
 
     order.update_customized_data(affiliate_data: "test")
     order.customized_data[:affiliate_data].should eq "test"
     order.customized_data[:facebook_email].should eq nil
+  end
+
+  it "should autodelete when destroyed" do
+    user_id = user.id
+    user.destroy
+
+    UserData.joins(:data_key).where(resource_id: user_id, user_data_keys: {name: "facebook_email"}).first.should eq nil
   end
 end
